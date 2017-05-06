dce5ff55b9a837356088e00cd7b87ced99d06e8e
Salem Velić <Salem Veli>
2017-05-06 22:42:54 +0200

    initial commit

---
+++ vue-cli/src/components/UserEdit.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="component">
+        <h3>You may edit the User here</h3>
+        <p>Edit me!</p>
+        <p>User age: {{ userAge }}</p>
+        <button @click="editAge">Edit Age</button>
+    </div>
+</template>
+
+<script>
+    import { eventBus } from '../main';
+
+    export default {
+        props: ['userAge'],
+        methods: {
+            editAge() {
+                this.userAge = 27;
+                eventBus.$emit('ageWasEdited', this.userAge);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        background-color: lightgreen;
+    }
+</style>
